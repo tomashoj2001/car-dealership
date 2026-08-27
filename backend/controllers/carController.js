b50e233ed16f6c22e5420901595bc42ccea90609
Tomás Hojnadel <tomashojnadel@gmail.com>
2026-08-27 11:41:39 -0300

    feat: implement CRUD operations for car management in the API

--- backend/controllers/carController.js
+++ backend/controllers/carController.js
@@ -21,8 +21,30 @@ export const getAllCars = async (req, res) => {
   }
 };
 
+export const getCar = async (req, res) => {
+  console.log('GETTING CAR WITH ID', req.params.id);
+
+  try {
+    const car = await Car.findById(req.params.id);
+
+    if (!car) throw Error('No car was found with this ID');
+
+    res.status(200).json({
+      status: 'success',
+      data: { car },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
 export const createCar = async (req, res) => {
   console.log('CREATING A CAR', req.body);
+
   try {
     const newCar = await Car.create(req.body);
 
@@ -35,6 +57,51 @@ export const createCar = async (req, res) => {
     res.status(400).json({
       status: 'fail',
       message: err,
+    });
+  }
+};
+
+export const updateCar = async (req, res) => {
+  console.log('UPDATING CAR WITH ID', req.params.id);
+
+  try {
+    const car = await Car.findByIdAndUpdate(req.params.id, req.body, {
+      returnDocument: 'after',
+      runValidators: true,
+    });
+
+    if (!car) throw Error('No car was found this ID');
+
+    res.status(200).json({
+      status: 'success',
+      data: { car },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
+export const deleteCar = async (req, res) => {
+  console.log('DELETING CAR WITH ID', req.params.id);
+
+  try {
+    const car = await Car.findByIdAndDelete(req.params.id);
+
+    if (!car) throw Error('No car was found with this ID');
+
+    res.status(201).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
     });
   }
 };
